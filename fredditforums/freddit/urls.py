41415e79df53fa321fc0a34b55f83478f5fb6229
YiboLiang <yiboliang02@gmail.com>
2022-09-07 15:46:00 -0700

    users are not able to add posts and threads

--- fredditforums/freddit/urls.py
+++ fredditforums/freddit/urls.py
@@ -1,8 +1,11 @@
+from unicodedata import name
 from django.urls import path
 from freddit import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_thread/', views.add_thread, name = 'add_thread'),
-    path('thread/<slug:thread_slug>/', views.view_thread, name="thread_post")
+    path('thread/<slug:thread_slug>/', views.view_thread, name="thread_post"),
+    path('thread/<thread_slug>/add_comment/', views.add_comment, name="add_comment"),
+
 ]
--- fredditforums/freddit/views.py
+++ fredditforums/freddit/views.py
@@ -1,5 +1,4 @@
 from http.client import HTTPResponse
-from multiprocessing import context
 from django.shortcuts import render
 from freddit.models import Thread, Comment
 from freddit.forms import ThreadForm, CommentForm
@@ -9,6 +8,25 @@ def index(request):
     thread_list = Thread.objects.order_by('-likes')[:5]
     context_dict = {'threads': thread_list}
     return render(request, 'freddit/index.html', context=context_dict)
+
+def add_comment(request, thread_slug):
+    try:
+        thread = Thread.objects.get(slug=thread_slug)
+    except Thread.DoesNotExist:
+        thread = None
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            if thread:
+                comment = form.save(commit=False)
+                comment.thread = thread
+                comment.likes = 0
+                comment.save()
+                return view_thread(request, thread_slug)
+        else:
+            print(form.errors)
+    return render(request, 'freddit/add_comment.html', {'comment': form, 'thread': thread})
 
 def add_thread(request):
     form = ThreadForm()
@@ -27,9 +45,9 @@ def view_thread(request, thread_slug):
         thread = Thread.objects.get(slug=thread_slug)
         comments = Comment.objects.filter(thread=thread)
         context_dict['thread'] = thread
-        context_dict['comment'] = comments        
+        context_dict['comments'] = comments        
     except Thread.DoesNotExist:
         context_dict['thread'] = None
-        context_dict['comment'] = None
+        context_dict['comments'] = None
     return render(request, 'freddit/thread.html', context_dict)
 
