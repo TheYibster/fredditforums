5e072c115a8efa0bb24796dbf32544b837558811
YiboLiang <yiboliang02@gmail.com>
2022-09-07 20:51:56 -0700

    generate new secret key; secured secret key

--- fredditforums/fredditforums/settings.py
+++ fredditforums/fredditforums/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e$llt+d==4kvk5%1e9a-9@j7f0vlk3lr1@rl2^i)_sa=t$)^n2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'freddit.apps.FredditConfig',
+    'django_extensions',
 
 ]
 
